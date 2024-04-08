75611dc466fb092951cd8186292292c3c72dc7ee
Stefan Haustein <stefan.haustein@gmail.com>
2024-04-08 23:45:19 +0200

    small cleanup

--- core/src/commonMain/kotlin/org/kobjects/greenspun/core/expr/Expr.kt
+++ core/src/commonMain/kotlin/org/kobjects/greenspun/core/expr/Expr.kt
@@ -40,31 +40,31 @@ abstract class Expr(vararg child: Any) {
 
     abstract val returnType: List<Type>
 
-    operator fun plus(other: Any) = returnType[0].createBinaryOperation(BinaryOperator.ADD, this, of(other))
-    operator fun minus(other: Any) = returnType[0].createBinaryOperation(BinaryOperator.SUB, this, of(other))
-    operator fun times(other: Any) = returnType[0].createBinaryOperation(BinaryOperator.MUL, this, of(other))
-    operator fun div(other: Any) = returnType[0].createBinaryOperation(BinaryOperator.DIV_S, this, of(other))
-    operator fun rem(other: Any) = returnType[0].createBinaryOperation(BinaryOperator.REM_S, this, of(other))
+    operator fun plus(other: Any) = returnType.first().createBinaryOperation(BinaryOperator.ADD, this, of(other))
+    operator fun minus(other: Any) = returnType.first().createBinaryOperation(BinaryOperator.SUB, this, of(other))
+    operator fun times(other: Any) = returnType.first().createBinaryOperation(BinaryOperator.MUL, this, of(other))
+    operator fun div(other: Any) = returnType.first().createBinaryOperation(BinaryOperator.DIV_S, this, of(other))
+    operator fun rem(other: Any) = returnType.first().createBinaryOperation(BinaryOperator.REM_S, this, of(other))
 
-    infix fun And(other: Any) = returnType[0].createBinaryOperation(BinaryOperator.AND, this, of(other))
-    infix fun Or(other: Any) = returnType[0].createBinaryOperation(BinaryOperator.OR, this, of(other))
-    infix fun Xor(other: Any) = returnType[0].createBinaryOperation(BinaryOperator.XOR, this, of(other))
+    infix fun And(other: Any) = returnType.first().createBinaryOperation(BinaryOperator.AND, this, of(other))
+    infix fun Or(other: Any) = returnType.first().createBinaryOperation(BinaryOperator.OR, this, of(other))
+    infix fun Xor(other: Any) = returnType.first().createBinaryOperation(BinaryOperator.XOR, this, of(other))
 
-    infix fun Shl(other: Any) = returnType[0].createBinaryOperation(BinaryOperator.SHL, this, of(other))
-    infix fun ShrS(other: Any) = returnType[0].createBinaryOperation(BinaryOperator.SHR_S, this, of(other))
+    infix fun Shl(other: Any) = returnType.first().createBinaryOperation(BinaryOperator.SHL, this, of(other))
+    infix fun ShrS(other: Any) = returnType.first().createBinaryOperation(BinaryOperator.SHR_S, this, of(other))
 
-    infix fun ShrU(other: Any) = returnType[0].createBinaryOperation(BinaryOperator.SHR_U, this, of(other))
+    infix fun ShrU(other: Any) = returnType.first().createBinaryOperation(BinaryOperator.SHR_U, this, of(other))
 
 
-    infix fun Rotr(other: Any) = returnType[0].createBinaryOperation(BinaryOperator.ROTR, this, of(other))
-    infix fun Rotl(other: Any) = returnType[0].createBinaryOperation(BinaryOperator.ROTR, this, of(other))
+    infix fun Rotr(other: Any) = returnType.first().createBinaryOperation(BinaryOperator.ROTR, this, of(other))
+    infix fun Rotl(other: Any) = returnType.first().createBinaryOperation(BinaryOperator.ROTR, this, of(other))
 
-    infix fun Eq(other: Any) = returnType[0].createRelationalOperation(RelationalOperator.EQ, this, of(other))
-    infix fun Ne(other: Any) = returnType[0].createRelationalOperation(RelationalOperator.NE, this, of(other))
-    infix fun Ge(other: Any) = returnType[0].createRelationalOperation(RelationalOperator.GE, this, of(other))
-    infix fun Gt(other: Any) = returnType[0].createRelationalOperation(RelationalOperator.GT, this, of(other))
-    infix fun Le(other: Any) = returnType[0].createRelationalOperation(RelationalOperator.LE, this, of(other))
-    infix fun Lt(other: Any) = returnType[0].createRelationalOperation(RelationalOperator.LT, this, of(other))
+    infix fun Eq(other: Any) = returnType.first().createRelationalOperation(RelationalOperator.EQ, this, of(other))
+    infix fun Ne(other: Any) = returnType.first().createRelationalOperation(RelationalOperator.NE, this, of(other))
+    infix fun Ge(other: Any) = returnType.first().createRelationalOperation(RelationalOperator.GE, this, of(other))
+    infix fun Gt(other: Any) = returnType.first().createRelationalOperation(RelationalOperator.GT, this, of(other))
+    infix fun Le(other: Any) = returnType.first().createRelationalOperation(RelationalOperator.LE, this, of(other))
+    infix fun Lt(other: Any) = returnType.first().createRelationalOperation(RelationalOperator.LT, this, of(other))
 
    /* infix fun GeU(other: Any) = returnType[0].createRelationalOperation(RelationalOperator.GE_U, this, of(other))
     infix fun GtU(other: Any) = returnType[0].createRelationalOperation(RelationalOperator.GT, this, of(other))
@@ -72,21 +72,21 @@ abstract class Expr(vararg child: Any) {
     infix fun LtU(other: Any) = returnType[0].createRelationalOperation(RelationalOperator.LT, this, of(other))
 */
 
-    fun DivU(other: Any) = returnType[0].createBinaryOperation(BinaryOperator.DIV_U, this, of(other))
-    fun RemU(other: Any) = returnType[0].createBinaryOperation(BinaryOperator.REM_U, this, of(other))
+    fun DivU(other: Any) = returnType.first().createBinaryOperation(BinaryOperator.DIV_U, this, of(other))
+    fun RemU(other: Any) = returnType.first().createBinaryOperation(BinaryOperator.REM_U, this, of(other))
 
 
 
     companion object {
         fun of(value: Any): Expr = if (value is Expr) value else Type.of(value).createConstant(value)
 
-        fun Not(value: Any): Expr = (if (value is Expr) value.returnType[0] else Type.of(value)).createUnaryOperation(UnaryOperator.NOT, Expr.of(value))
+        fun Not(value: Any): Expr = (if (value is Expr) value.returnType.first() else Type.of(value)).createUnaryOperation(UnaryOperator.NOT, Expr.of(value))
 
 
     }
 
-    fun Max(left: Expr, right: Any) = left.returnType[0].createBinaryOperation(BinaryOperator.MAX, left, of(right))
-    fun Min(left: Expr, right: Any) = left.returnType[0].createBinaryOperation(BinaryOperator.MIN, left, of(right))
+    fun Max(left: Expr, right: Any) = left.returnType.first().createBinaryOperation(BinaryOperator.MAX, left, of(right))
+    fun Min(left: Expr, right: Any) = left.returnType.first().createBinaryOperation(BinaryOperator.MIN, left, of(right))
 
 
     fun eval(context: LocalRuntimeContext): Any {
